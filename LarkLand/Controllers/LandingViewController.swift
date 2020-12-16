edd7d4eb73200822df3a9c871d7cc5e685e4d861
hossein.shirinabadi <hossein.shirinabadi@bytedance.com>
2020-12-16 15:30:36 -0800

    modified scene file and removed the storyboard

---
+++ LarkLand/Controllers/LandingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LandingViewController.swift
+//  LarkLand
+//
+//  Created by Hossein on 12/16/20.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
